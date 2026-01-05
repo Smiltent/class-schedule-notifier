40c22792cad898d403af895ba1d3e4d5acf1fded
Smil <me@smilt.dev>
2026-01-05 23:39:08 +0200

    resising support, for better images

--- public/download.js
+++ public/download.js
@@ -1,17 +1,34 @@
-
 import { settings } from "/public/lookup.js"
 
 const button = document.getElementById("downloadBtn")
 const table = document.getElementById("tableContainer")
 
 button.addEventListener("click", async () => {
+    const width = 1400
+
+    const ogStyles = {
+        width: table.style.width,
+        transform: table.style.transform,
+        transformOrigin: table.style.transformOrigin
+    }
+
+    table.style.width = `${width}px`
+    table.style.transform = "scale(1)"
+    table.style.transformOrigin = "top left"
+
+    await new Promise(requestAnimationFrame)
+
     const canvas = await window.html2canvas(table, {
-        scale: 2,
+        scale: 1,
         useCORS: true,
         backgroundColor: null
     })
 
     const image = canvas.toDataURL("image/png")
+
+    table.style.width = ogStyles.width
+    table.style.transform = ogStyles.transform
+    table.style.transformOrigin = ogStyles.transformOrigin
 
     const link = document.createElement("a")
     link.href = image
