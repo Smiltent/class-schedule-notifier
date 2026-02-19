9588e36cb2bf033dad87fcd27d4f59d43ca05017
Smil <me@smilt.dev>
2026-02-19 22:52:54 +0200

    ...

--- src/db/RawScheduleData.ts
+++ src/db/RawScheduleData.ts
@@ -1,13 +1,6 @@
 
 import mongoose, { Schema } from 'mongoose'
 
-// I realised, I need to parse over the old schema to the new one
-// export default mongoose.model('RawScheduleData', new Schema({
-//     week: { type: Schema.Types.ObjectId, ref: "Week", required: true },
-//     data: { type: Schema.Types.Mixed, required: true }
-// }))
-
-// But this works fine... for now...
 export default mongoose.model('RawScheduleData', new Schema({
     week: { type: String, required: true },
     data: { type: Schema.Types.Mixed, required: true }
